1b96c0866c7021e67ff57d70ac28b3f5f12cc594
Andrey Bondarenko <abondarenko@weezlabs.com>
2016-03-11 10:35:48 +0300

    Initial Commit

---
+++ SimpleTwitterClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTwitterClient
+//
+//  Created by Andrey Bondarenko on 11.03.16.
+//  Copyright © 2016 Rus Wizards Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTwitterClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTwitterClient
+//
+//  Created by Andrey Bondarenko on 11.03.16.
+//  Copyright © 2016 Rus Wizards Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
